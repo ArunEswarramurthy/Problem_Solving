bea85b68c85f2ab51bd1b86b7c46c5d94180e0f6
ArunEswarramurthy <aruneswarramurthy@gmail.com>
2025-12-09 04:03:35 +0000

    Array Problems

---
+++ Array_Problems/largestnumber.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class largestnumber{
+
+ public static  int getSecondLargest(int[] arr) {
+        
+        int max = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        for(int i =0;i<arr.length ; i++){
+            if(arr[i] > max){
+                max2 = max;
+                max = arr[i];
+                
+            }
+            else if(arr[i] <  max && arr[i] > max2){
+                max2 = arr[i];
+            }
+            
+        }
+        
+        
+         if(max2 == Integer.MIN_VALUE ){
+            return -1;
+        }
+        else{
+           return max2;
+        }
+        
+    }
+
+
+    public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+int n = sc.nextInt();
+int[] arr = new int[n];
+for(int i =0;i<n;i++){
+    arr[i] = sc.nextInt();
+}
+System.out.println(getSecondLargest(arr));
+
+    }
+}
